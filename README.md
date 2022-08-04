21e3ef163ed69e8e5da23e7a7a5f3aa97879b593
Clownacy <Clownacy@users.noreply.github.com>
2022-08-04 21:34:48 +0100

    Drop the public-domain dedication.
    I'm not entirely sure that my public-domain dedication is recognised
    legally, so I'm just going to go with BSD-0 exclusively since it
    should work anywhere and have the same effect aside from me
    technically retaining copyright ownership.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # clownresampler
 
 This is a single-file library for resampling audio. It is written in C89 and
-dual-licenced under the terms of The Unlicence and the Zero-Clause BSD licence.
+licensed under the terms of the Zero-Clause BSD licence.
+
 In particular, this library implements a windowed-sinc resampler, using a
 Lanczos window.
--- clownresampler.h
+++ clownresampler.h
@@ -2,7 +2,8 @@
 clownresampler
 
 This is a single-file library for resampling audio. It is written in C89 and
-dual-licenced under the terms of The Unlicence and the Zero-Clause BSD licence.
+licensed under the terms of the Zero-Clause BSD licence.
+
 In particular, this library implements a windowed-sinc resampler, using a
 Lanczos window.
 
@@ -10,39 +11,6 @@ https://github.com/Clownacy/clownresampler
 */
 
 /*
-This software is available under two licences - choose whichever you prefer:
-
-
-Licence 1 - The Unlicense
-
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non - commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org/>
-
-
-Licence 2 - Zero-Clause BSD
-
 Copyright (c) 2022 Clownacy
 
 Permission to use, copy, modify, and/or distribute this software for any
